4695c95efb2db3cc833c49504207b77657ca6064
Pellinore <96124363+Pell@users.noreply.github.com>
2025-08-14 17:01:44 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,20 @@
 
 <template>
   <header>
-    <h1> Hello CAT </h1>
+    <div class="container">
+      <img src="./assets/[icon].png" alt="">
+      <div>
+        <img src="./assets/logo.png" alt="">
+        <div class="container container__vertical text">
+          <p>+7 962 253 20 37</p>
+          <p>sales@skillline.ru</p>
+          <p>skillline.ru</p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h1>Тестовое задание</h1>
+    </div>
   </header>
 
   <main>
